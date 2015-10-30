3bfe307fffc2878b67602a9c946c1777b6f94a41
Joel Lehtonen <jlehtonen@prasos.fi>
2015-10-30 20:34:57 +0200

    Remove unnecessary dependencies

--- project.clj
+++ project.clj
@@ -5,12 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"bitcoinj" "http://distribution.bitcoinj.googlecode.com/git/releases/"}
   :dependencies [[org.bitcoinj/bitcoinj-core "0.12.3"]
-                 [com.madgag/scprov-jdk15on "1.47.0.3"]
-                 [org.clojars.dbasch/bip38 "0.1.0"]
-                 [com.google.guava/guava "15.0"]
                  [org.slf4j/slf4j-simple "1.7.5"]
-                 [com.google.zxing/core "2.3.0"]
-                 [com.google.zxing/javase "2.3.0"]
                  [org.clojure/clojure "1.5.1"]
                  [pandect "0.5.2"]]
   :main bitcoin.miniwallet)
