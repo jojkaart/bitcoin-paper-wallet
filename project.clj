a384c07d498e0b3a345995f4753db2196653e610
Joel Lehtonen <jlehtonen@prasos.fi>
2016-04-19 13:04:18 +0300

    Upgrade library versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"bitcoinj" "http://distribution.bitcoinj.googlecode.com/git/releases/"}
-  :dependencies [[org.bitcoinj/bitcoinj-core "0.12.3"]
-                 [org.slf4j/slf4j-simple "1.7.5"]
-                 [org.clojure/clojure "1.5.1"]
-                 [pandect "0.5.2"]]
+  :dependencies [[org.bitcoinj/bitcoinj-core "0.13.6"]
+                 [org.slf4j/slf4j-simple "1.7.21"]
+                 [org.clojure/clojure "1.8.0"]
+                 [pandect "0.5.4"]]
   :main bitcoin.miniwallet)
