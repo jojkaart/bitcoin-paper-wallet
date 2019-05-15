f56de5f7d8d7288aa856cf911d4dcaf9cd83f42c
Joel Lehtonen <jlehtonen@prasos.fi>
2019-05-15 15:01:50 +0300

    Update library version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bitcoin "0.1.0-SNAPSHOT"
+(defproject bitcoin "0.1.1-SNAPSHOT"
   :description "offline paper wallet generator"
   :url "http://diegobasch.com"
   :license {:name "Eclipse Public License"
